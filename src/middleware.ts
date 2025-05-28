2763be06e6465268a23a36b36b1446608c08af5e
QUANGHPD-PC\chau.mdk <chau.mdk@aris-vn.com>
2025-05-28 10:36:53 +0700

    ref #Tao chat tương tác Giữa người dùng và admin-- fix bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,7 @@ export async function middleware(req: NextRequest) {
   if (
     pathname.startsWith('/client') ||
     pathname.startsWith('/api/messages') ||
+    pathname.startsWith('/api/product') ||
     pathname.startsWith('/login') ||
     pathname.startsWith('/register') ||
     pathname.startsWith('/api/auth') ||
